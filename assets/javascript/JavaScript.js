de213e2a6139448731f689287d0677560a4d5207
Alex Betancourt <alex@Alexs-MacBook-Pro.local>
2021-11-25 12:25:01 -0500

    added filters to limit the amount of data returned

--- assets/javascript/JavaScript.js
+++ assets/javascript/JavaScript.js
@@ -1,9 +1,6 @@
-// in the index file we need a ul element - For Search History:
-// var searchHistoryLocation = document.querySelector("ul");
-
 var runSearch = function () {
-    // search bar on index file needs an id of "search-bar"
     var locationRequested = document.getElementById("locale").value;
+    // to keep all the request organized in the console 
     console.log(locationRequested);
     var requestUrl = "https://api.opentripmap.com/0.1/en/places/geoname?name=" + locationRequested + "&apikey=5ae2e3f221c38a28845f05b646d21e5f2e56e0429c8c0d446cc8bae6";
 
@@ -14,23 +11,16 @@ var runSearch = function () {
         .then(function(data) {
             var lon = data.lon;
             var lat = data.lat;
-            var requestUrlRadius = "https://api.opentripmap.com/0.1/en/places/radius?radius=8041&lon=" + lon + "&lat=" + lat + "&format=json&apikey=5ae2e3f221c38a28845f05b646d21e5f2e56e0429c8c0d446cc8bae6";
+            // In the future we can add more "kinds" (filters) to get other data such as "natural" and "tourist facilities" (tourist_facilities%2Cnatural%2C)
+            var requestUrlRadius = "https://api.opentripmap.com/0.1/en/places/radius?radius=10000&lon=" + lon + "&lat=" + lat + "&kinds=cultural%2Camusements&rate=1&format=json&apikey=5ae2e3f221c38a28845f05b646d21e5f2e56e0429c8c0d446cc8bae6";
             fetch(requestUrlRadius)
                 .then(function(response) {
                     return response.json();
                 })
-            
+            // to be able to see links in console and have visibility to what information we can access for further development
             console.log(requestUrlRadius);
-            console.log(requestUrl);
-    
-        
         })
-    
-
-    // create a search history under the search bar BELOW:
-    // var listItemLocation = document.createElement('h4');
-    // listItemLocation.textContent = locationRequested;
-    // searchHistoryLocation.appendChild(listItemCity);
-    }
-
+    // convert distance from meters to miles
+    // var distance = data.dist * 0.00062137
+}
 $("#search-button").on("click", runSearch);
