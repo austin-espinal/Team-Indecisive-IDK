577dc35c1f9299d252f4452ac9425e7503825491
austin espinal <austinespi@gmail.com>
2021-11-28 04:47:54 -0500

    add new materialize side menu function to both index and results page. updated css to both

--- assets/javascript/JavaScript.js
+++ assets/javascript/JavaScript.js
@@ -1,14 +1,3 @@
-$(document).ready(function () {
-    $('.sidenav').sidenav();
-});
-//   <nav>
-//             <div class="nav-wrapper blue-grey darken-4">
-//                 <a href="#!" class="brand-logo center"><i class="material-icons">room_service</i>Concierge</a>
-//                 <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-//                 <ul class="right hide-on-med-and-down">
-//                     <li id="save-menu-btn"><a data-target="slide-out"
-//                             class="waves-effect waves-light btn-flat sidenav-trigger"><i
-//                                 class="material-icons">menu</i></a></li>
-//                 </ul>
-//             </div>
-//         </nav>
+//Side menu initialized
+const sideNav = document.querySelector('.sidenav');
+M.Sidenav.init(sideNav, {});
